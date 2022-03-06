3d92c1bfca26e454ae41f2139533c06f80cdce11
Shop-kins <silver7392@hotmail.co.uk>
2022-03-06 12:38:55 +0000

    Create TransitionHandler.cs

---
+++ Player Events/TransitionHandler.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace TransitionRandomiser.Player_Events
+{
+    internal class TransitionHandler
+    {
+
+        private static String CurrentBiome = "lifepod";
+        private static Boolean Processing = false;
+        private static Dictionary<Array, TeleportPosition> TransitionDictionary = new Dictionary<Array, TeleportPosition>()
+        {
+        };
+        internal static void SetCurrentBiome(String Biome)
+        {
+            CurrentBiome = Biome;
+        }
+
+        internal static String GetCurrentBiome()
+        {
+            return CurrentBiome;
+        }
+
+        internal static Boolean GetProcessing()
+        {
+            return Processing;
+        }
+
+        internal static void SetProcessing(Boolean state)
+        {
+            Processing = state;
+        }
+
+        internal static TeleportPosition getTeleportPositionForBiomeTransfer(String newBiome)
+        {
+
+            String[] biomeTransfer = { CurrentBiome, newBiome };
+            if (TransitionDictionary.ContainsKey(biomeTransfer))
+            {
+                return TransitionDictionary[biomeTransfer];
+            }
+            else
+            {
+                var biomePosition = GetRandomBiomePositions();
+                TransitionDictionary.Add(biomeTransfer, biomePosition);
+                return biomePosition;
+            }
+        }
+
+        private static TeleportPosition GetRandomBiomePositions()
+        {
+            var transitionLocations = TransitionDictionary.Values.ToArray();
+            var biomeTeleportData = BiomeConsoleCommand.main.data.locations;
+            var availableLocations = biomeTeleportData.Except(transitionLocations).ToArray();
+            var totalCount = availableLocations.Length;
+            var newPositionNum = Random.Range(0, totalCount);
+
+            return availableLocations[newPositionNum];
+        }
+    }
+}
